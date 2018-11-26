a1331fcb7d4d1c9adedac3cfb542bd3ea6dee729
manhtruong594 <manhtruong594@gmail.com>
2018-11-27 01:47:20 +0700

    first commit

---
+++ Session01/homework/TurtleEx/TurtleEx1.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+speed(1)
+color("red")
+right(60)
+
+for i in range(4):
+    forward(50)
+    right(60)
+    forward(50)
+    right(120)
+    forward(50)
+    right(60)
+    forward(50)
+    right(30)
+
+
+mainloop()
---
+++ Session01/homework/TurtleEx/TurtleEx2.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+speed(5)
+
+color("red")
+
+for i in range(4):
+    left(90)
+    forward(100)
+
+for i in range(6):
+    left(60)
+    forward(100)
+
+color("blue")
+
+for i in range(2):
+    left(120)
+    forward(100)
+
+right(132)
+for i in range(4):
+    forward(100)
+    right(72)
+    
+
+mainloop()
